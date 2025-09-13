9202f215d5a7f6357e896f8bf8285028855feae6
Atharva Iyer <atharvaiyer2006@gmail.com>
2025-09-13 05:16:55 +0000

    401: The Workstation does not exist or your currently signed in account

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,6 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 
-const firebaseConfig = {
-  "projectId": "studio-498700326-2afa2",
-  "appId": "1:639279667384:web:8c087a0cdc4546e8667c4d",
-  "storageBucket": "studio-498700326-2afa2.firebasestorage.app",
-  "apiKey": "AIzaSyDosAwz4or-TVXDLcAu3kEy0IcZUCa5baI",
-  "authDomain": "studio-498700326-2afa2.firebaseapp.com",
-  "messagingSenderId": "639279667384"
-};
+const firebaseConfig = {};
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
